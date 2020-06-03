d4ad37e2cabaa9503799c48a2bb33e52809d7bc6
nandani aggarwal <nandani.aggarwal@ausmel117366lt.modem>
2020-06-03 12:28:53 +1000

    Checking webhook triggers

--- src/main/java/com/nbn/demo/controller/HelloController.java
+++ src/main/java/com/nbn/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping(value = "hello")
     public String sayHello(){
-        System.out.println("Hello world controller");
+        System.out.println("Hello world new controller");
         return "Hello World";
     }
 }
